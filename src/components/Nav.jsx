276dc9ddcbfdd522e1262d52910acd8f8acc1c12
VALiUM <srujansamal1@gmail.com>
2023-10-05 19:19:35 +0530

    fixed some minor bugs

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -9,6 +9,13 @@ const Nav = () => {
   // Function to toggle the menu state
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
+    
+    // Disable or enable scrolling on the body based on menu state
+    if (!isMenuOpen) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = 'unset';
+    }
   };
 
   return (
@@ -41,30 +48,30 @@ const Nav = () => {
             style={{ cursor: "pointer" }}
           />
         </div>
-		<div className="">
-      {isMenuOpen && (
-        <div className='sidebar-menu bg-opacity-95 items-center absolute flex flex-col  justify-center  mt-2 mb-6  h-screen bg-white w-3/4 right-0'>
-          <ul>
-            {navLinks.map((item) => (
-              <li key={item.label} className="my-4">
-                <a
-                  href={item.href}
-                  className='font-montserrat  font-bold  hover:text-gray-500 text-2xl leading-normal  text-gray-900'
-                >
-                  {item.label}
-                </a>
-              </li>
-            ))}
-          </ul>
+        
+        <div className="">
+          {isMenuOpen && (
+            <div className='sidebar-menu bg-opacity-95 items-center absolute flex flex-col  justify-center  mt-2 mb-6  h-screen bg-white w-3/4 right-0'>
+              <ul>
+                {navLinks.map((item) => (
+                  <li key={item.label} className="my-4">
+                    <a
+                      href={item.href}
+                      className='font-montserrat  font-bold  hover:text-gray-500 text-2xl leading-normal  text-gray-900'
+                    >
+                      {item.label}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          )}
         </div>
-      )}
-	  </div>
-
       </nav>
 
       {/* Sidebar Menu */}
-	  
-      <span className='text-center xl:bg-white text-2xl bg-gradient-to-r from-blue-500 via-purple-500 to-red-500 bg-clip-text text-transparent xl:whitespace-nowrap relative z-10 pr-10'>
+      
+      <span className='text-center xl:bg-white text-2xl bg-gradient-to-r from-blue-500 via-purple-500 to-red-500 bg-clip-text text-transparent  '>
         Complete Mechanical Solution.
       </span>
     </header>
